167e9779cb4f655714ff1f6b3f9744a6f1cb6c2e
root <stefan.hadamek@fau.de>
2023-01-15 23:47:01 +0100

    included one last test

--- src/test/java/org/wahlzeit/model/F1CarsPhotoManagerTest.java
+++ src/test/java/org/wahlzeit/model/F1CarsPhotoManagerTest.java
@@ -14,16 +14,18 @@ public class F1CarsPhotoManagerTest{
     public void init(){
         rset = mock(ResultSet.class);
     }
-    /*
+    
     @Test 
     public void testReadOn() throws SQLException {
-        when(rset.getString("car_model")).thenReturn("rbv2");
+        when(rset.getString("car_model_generation")).thenReturn("3");
+        when(rset.getString("car_model_machine_type")).thenReturn("rb_engine");
         when(rset.getString("owner_email_address")).thenReturn("totto@wolff.com");
         when(rset.getString("owner_home_page")).thenReturn("http://redbull.com");
         F1CarsPhoto ph = F1CarsPhotoManager.getManager().createObject(rset);
-        Assert.assertEquals("rbv2", ph.getF1CarsPhotoModel());
+        Assert.assertEquals("3", ph.getF1Car().getGeneration());
+        Assert.assertEquals("rb_engine", ph.getF1Car().getMachineType().getMachineType());
     }
-    */
+    
 
 }
 
